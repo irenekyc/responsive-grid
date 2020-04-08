87b0b4dbb205035842bde26ea41ccf1f19c9cf89
irenekyc <gugu_irene@hotmail.com>
2020-04-08 13:43:11 -0500

    upload

---
+++ app.js
@@ -0,0 +1,7 @@
+const menuWidth = getComputedStyle(document.querySelector('#menu-1'))
+const menuCard = document.querySelector('#menu-4')
+
+const container = document.querySelector('.menu')
+
+console.log(getComputedStyle(container).height)
+console.log(menuCard.scrollHeight)
